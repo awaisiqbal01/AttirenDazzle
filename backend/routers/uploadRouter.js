5be8caaf664a432a1ac1b320e7b16f4bac9ad422
awaissadozai <awais@onescreensolutions.com>
2023-10-24 13:16:42 +0500

    working attiredazzle

---
+++ backend/routers/uploadRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const multer = require('multer');
+const { isAuth, isAdmin } = require('../utils');
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename(req, file, cb) {
+    cb(null, `${Date.now()}.jpg`);
+  },
+});
+
+const upload = multer({ storage });
+const uploadRouter = express.Router();
+
+uploadRouter.post('/', isAuth, isAdmin, upload.single('image'), (req, res) => {
+  res.status(201).send({ image: `/${req.file.path}` });
+});
+module.exports =  uploadRouter;
